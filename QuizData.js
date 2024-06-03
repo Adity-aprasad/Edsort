8a11418ce5903f0b653a20e195533c7825cb4aff
Adity-aprasad <141990373+Adity-aprasad@users.noreply.github.com>
2024-06-03 22:43:01 +0530

    Add files via upload

---
+++ QuizData.js
@@ -0,0 +1,28 @@
+
+    
+    export const QuizData = [
+        {
+            question: "A relation R in a set A is called , if (a1, a2) ∈ R implies (a2, a1) ∈ R,for all a1, a2 ∈ A ?",
+            options:[ "symmetric", "transitive", "equivalence", " non-symmetric"],
+            answer: 1
+        },
+        {
+            question: "relation R in a set A is called _______, if (a1, a2) ∈ R implies (a2, a1) ∈ R, for all a1, a2 ∈ A.",
+            options:["symmetric","transitive" ,"equivalence", "non-symmetric"],
+            answer: 1
+        },
+        {
+            question: " Let R be a relation on the set N of natural numbers defined by nRm if n divides m. Then R is",
+            options: ["Reflexive and symmetric"," Transitive and symmetric","Equivalence","Reflexive, transitive but not symmetric"],
+            answer: 4
+        },
+        {
+            question: "The maximum number of equivalence relations on the set A = {1, 2, 3} are",
+            options: ["1"," 2","3","5"],
+            answer:4
+        },
+        {
+            
+        }
+    ];
+
